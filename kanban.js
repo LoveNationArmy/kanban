e509ebe13f568007184c00d953a505f7643ab181
George Stagas <gstagas@gmail.com>
2019-06-19 15:46:29 +0200

    create cards in columns

--- kanban.js
+++ kanban.js
@@ -8,15 +8,36 @@ function createColumn (title) {
   }
 
   columns.push(column)
+
+  return column
+}
+
+function createCard (content) {
+  const parts = content.split('\n')
+
+  const card = {
+    title: parts[0],
+    content: parts.slice(1).join('\n') || ''
+  }
+
+  return card
 }
 
 function renderToHTML () {
   let html = '<div class="kanban">'
 
   columns.forEach(column => {
+    const cards = column.cards.map(card => `
+      <div class="kanban-card">
+        <div class="kanban-card-title">${card.title}</div>
+        <div class="kanban-card-content">${card.content}</div>
+      </div>
+    `)
+
     html += `
       <div class="kanban-column">
         <div class="kanban-column-title">${column.title}</div>
+        <div class="kanban-column-cards">${cards.join('\n')}</div>
       </div>
     `
   })
@@ -24,9 +45,12 @@ function renderToHTML () {
   return html + '</div>'
 }
 
-createColumn('backlog')
+let backlog = createColumn('backlog')
 createColumn('todo')
 createColumn('in progress')
 createColumn('done')
+
+let card = createCard('Hello, world\nGreetings\npeople\nThis is a card and it has some quite long content that should wrap.')
+backlog.cards.push(card)
 
 container.innerHTML = renderToHTML()
