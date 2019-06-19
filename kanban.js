f93fd44d30378f44c3efd7bb75e730e416213f43
George Stagas <gstagas@gmail.com>
2019-06-19 23:13:29 +0200

    cleanup

--- kanban.js
+++ kanban.js
@@ -1,112 +1,108 @@
 import morphdom from './morphdom.js'
 
-let columnId = 0
+// ids are incrementally numbered as a singleton
+// in order to avoid id collisions
+let kanbanId = 0
 
-const columns = []
+/**
+ * Kanban
+ */
 
-function createColumn (title = 'untitled') {
-  const column = {
-    id: `column_${columnId++}`,
-    title: title,
-    cards: []
+export default class Kanban {
+  constructor (el) {
+    this.id = kanbanId++
+    this.el = el
+    this.columns = []
+    this.dropListener = this.dropListener.bind(this)
+    window.addEventListener('message', this.dropListener)
   }
 
-  columns.push(column)
-
-  return column
-}
-
-function createCard (content = '') {
-  const parts = content.split('\n')
-
-  const card = {
-    title: parts[0],
-    content: parts.slice(1).join('\n')
+  destroy () {
+    window.removeEventListener('message', this.dropListener)
   }
 
-  return card
-}
+  render () {
+    morphdom(this.el, this.renderToHTML())
+  }
 
-function renderToHTML () {
-  let html = '<div class="kanban">'
+  renderToHTML () {
+    let html = `<div data-id="${this.id}" class="kanban">`
 
-  columns.forEach(column => {
-    const cards = column.cards.map((card, index) => `
-      <div id="${column.id}_${index}" class="kanban-card" draggable="true" ondragstart="Kanban.dragStart(event)">
-        <div class="kanban-card-title">${card.title}</div>
-        <div class="kanban-card-content">${card.content}</div>
-      </div>
-    `)
+    this.columns.forEach(column => {
+      const cards = column.cards.map((card, index) => `
+        <div data-index="${index}" class="kanban-card" draggable="true" ondragstart="Kanban.dragStart(event)">
+          <div class="kanban-card-title">${card.title}</div>
+          <div class="kanban-card-content">${card.content}</div>
+        </div>
+      `)
 
-    html += `
-      <div id="${column.id}" class="kanban-column" ondrop="Kanban.drop(event)" ondragover="Kanban.dragOver(event)">
-        <div class="kanban-column-title">${column.title}</div>
-        <div class="kanban-column-cards">${cards.join('\n')}</div>
-      </div>
-    `
-  })
+      html += `
+        <div data-id="${column.id}" class="kanban-column" ondrop="Kanban.drop(event)" ondragover="Kanban.dragOver(event)">
+          <div class="kanban-column-title">${column.title}</div>
+          <div class="kanban-column-cards">${cards.join('\n')}</div>
+        </div>
+      `
+    })
 
-  return html + '</div>'
-}
+    return html + '</div>'
+  }
 
-window.Kanban = {
-  dragStart (event) { // TODO: { dataTransfer, card: target }?
+  createColumn (title = 'untitled') {
+    const column = {
+      id: this.columns.length,
+      title: title,
+      cards: []
+    }
+
+    this.columns.push(column)
+
+    return column
+  }
+
+  createCard (content = '') {
+    const parts = content.split('\n')
+
+    const card = {
+      title: parts[0],
+      content: parts.slice(1).join('\n')
+    }
+
+    return card
+  }
+
+  dropListener (event) {
+    if (event.data.kanbanId == this.id) {
+      const card = this.columns[event.data.sourceColumnId].cards.splice(event.data.cardIndex, 1)[0]
+      this.columns[event.data.targetColumnId].cards.push(card)
+      this.render()
+    }
+  }
+
+  static dragStart (event) {
     event.dataTransfer.effectAllowed = 'move'
-    event.dataTransfer.setData('cardId', event.target.id)
-    event.dataTransfer.setData('columnId', event.target.closest('.kanban-column').id.split('_').pop())
-  },
+    event.dataTransfer.setData('kanbanId', event.target.closest('.kanban').dataset.id)
+    event.dataTransfer.setData('columnId', event.target.closest('.kanban-column').dataset.id)
+    event.dataTransfer.setData('cardIndex', event.target.dataset.index)
+  }
 
-  dragOver (event) {
+  static dragOver (event) {
     event.preventDefault()
     event.dataTransfer.dropEffect = 'move'
-  },
+  }
 
-  drop (event) {
+  static drop (event) {
     event.preventDefault()
 
-    const cardId = event.dataTransfer.getData('cardId')
-    const cardIndex = cardId.split('_').pop()
+    const kanbanId = event.dataTransfer.getData('kanbanId')
     const sourceColumnId = event.dataTransfer.getData('columnId')
-    const targetColumnId = event.target.closest('.kanban-column').id.split('_').pop()
+    const sourceCardIndex = event.dataTransfer.getData('cardIndex')
+    const targetColumnId = event.target.closest('.kanban-column').dataset.id
 
-    const card = columns[sourceColumnId].cards.splice(cardIndex, 1)[0]
-    columns[targetColumnId].cards.push(card)
-
-    morphdom(container.firstChild, renderToHTML())
-    // container.innerHTML = renderToHTML()
-
-    // let target = event.target.closest('.kanban-column').querySelector('.kanban-column-cards')
-    // target.appendChild(document.getElementById(data));
+    window.postMessage({
+      kanbanId,
+      sourceCardIndex,
+      sourceColumnId,
+      targetColumnId
+    })
   }
 }
-
-let backlog = createColumn('backlog')
-let todo = createColumn('todo')
-let in_progress = createColumn('in progress')
-let done = createColumn('done')
-
-let card = createCard('Hello, world\nGreetings\npeople\nThis is a card and it has some quite long content that should wrap.')
-backlog.cards.push(card)
-backlog.cards.push(card)
-backlog.cards.push(card)
-backlog.cards.push(card)
-backlog.cards.push(card)
-backlog.cards.push(card)
-backlog.cards.push(card)
-todo.cards.push(card)
-todo.cards.push(card)
-todo.cards.push(card)
-in_progress.cards.push(card)
-done.cards.push(card)
-done.cards.push(card)
-done.cards.push(card)
-done.cards.push(card)
-done.cards.push(card)
-done.cards.push(card)
-done.cards.push(card)
-done.cards.push(card)
-done.cards.push(card)
-done.cards.push(card)
-done.cards.push(card)
-
-morphdom(container.firstChild, renderToHTML())
